b2fa8f8fc389ad4e0710f1a83335f65757359a98
RajivRnjan <114426029+RajivRnjan@users.noreply.github.com>
2023-07-18 10:59:38 +0530

    18 july

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import {Link} from "react-router-dom";
+
+import logo from "../Images/logo.svg";
+
 
 function Navbar() {
+  const gotoTopWindow = () => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+      };
   return (
-    <div>Navbar</div>
+    <div id="navbar">
+       <div className="nav-left">
+         <Link to="/">
+           <div>
+             <img src={logo} className="nav-logo" alt="logo" />
+           </div>
+         </Link>
+    </div>
+
+    <div className="nav-right">
+      <ul className="nav-items">
+        <li onClick={gotoTopWindow}><Link to="/home">Home</Link></li>
+        <li onClick={gotoTopWindow}><Link to="/AboutUs">About</Link></li>
+        <li onClick={gotoTopWindow}><Link to="/Signup">Signup</Link></li>
+        {/* <li onClick={gotoTopWindow}><Link to="/Login">Login</Link></li> */}
+        <li onClick={gotoTopWindow}><Link to="/ContactUs">Contact</Link></li>
+
+      </ul>
+    </div>
+    </div>
   )
 }
 
