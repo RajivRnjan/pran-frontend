d327b31a12b8ec780dce0b8f8218a14e559d0d7f
RajivRnjan <114426029+RajivRnjan@users.noreply.github.com>
2023-08-03 21:19:19 +0530

    signup

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,5 +1,9 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+
+import Spinner from "../Spinner";
 import LoginSignupHome from "./LoginSignupHome";
 import "../../App.css";
 import Crousel from "../Crousel";
@@ -10,40 +14,132 @@ import { MdSmartphone, MdSchool, MdEmail } from "react-icons/md";
 
 function Signup() {
   const navigate = useNavigate();
-
   const navigateLogin = () => {
     navigate("/login");
   };
+
+  const numberHandler = (event) => {
+    event.preventDefault();
+    let number = event.target.value;
+    let numberStr = number.toString();
+
+    if (numberStr.length < 10 || number < 6000000000 || number >= 9999999999) {
+      document.querySelector("#number").style.borderBottom = "1px solid red";
+    } else {
+      document.querySelector("#number").style.borderBottom =
+        "1px solid #5B5B5B";
+    }
+  };
+  const emailHandler = (event) => {
+    event.preventDefault();
+    let email = event.target.value;
+    let splitEmail = email.split("@");
+
+    if (splitEmail[1] !== "gmail.com") {
+      document.querySelector("#email").style.borderBottom = "1px solid red";
+    } else {
+      document.querySelector("#email").style.borderBottom = "1px solid #5B5B5B";
+    }
+  };
+
+  // var password = "";
+  // if (document.getElementById('password') != null){
+  // var password = document.getElementById("password").value;
+  // }
+  // // console.log(password)
+
+  // const passwordHandler = (event)=>{
+  //   event.preventDefault();
+
+  //   let cpassword = event.target.value;
+
+  //   if(password === cpassword){
+  //     document.querySelector("#cpassword").style.border="1px solid red";
+  //   }
+  //   else{
+  //     document.querySelector("#cpassword").style.border="none";
+  //   }
+  // }
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (event) => {
+    setLoading(true);
+    event.preventDefault();
+    const name = event.target.name.value;
+    const number = event.target.number.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    const cpassword = event.target.cpassword.value;
+    const college = event.target.college.value;
+    const course = event.target.course.value;
+    const sem = event.target.sem.value;
+
+    if (password === cpassword) {
+      axios
+        .post("https://pran-app-backend.onrender.com/api/user/signup", {
+          name,
+          number,
+          email,
+          password,
+          college,
+          course,
+          sem,
+        })
+        .then((response) => {
+          console.log(response);
+          alert(response.data.msg);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.log(error);
+          setLoading(false);
+        });
+    } else {
+      alert("Password Mismatched");
+      setLoading(false);
+    }
+  };
+
   return (
     <>
+      {loading ? <Spinner /> : ""}
       <div className="background-img">
         <div className="top-container">
           <div className="signup-container">
-            <div class="signup-form">
+            <div className="signup-form">
               <h2>Sign Up</h2>
-              <form>
+
+              <form onSubmit={handleSubmit}>
                 <div className="input-container">
                   <IoMdContact size="25px" color="#5B5B5B" />
                   <input
                     type="text"
                     placeholder="Enter student name"
                     name="name"
+                    required
                   />
                 </div>
-                <div className="input-container">
+                <div className="input-container" id="number">
                   <MdSmartphone size="25px" color="#5B5B5B" />{" "}
                   <input
                     type="number"
                     placeholder="Enter Mobile No"
                     name="number"
+                    pattern="[6789][0-9]{9}"
+                    title="Please enter valid phone number"
+                    required
+                    onChange={numberHandler}
                   />
                 </div>
-                <div className="input-container">
+                <div className="input-container" id="email">
                   <MdEmail size="25px" color="#5B5B5B" />
                   <input
                     type="email"
                     placeholder="Enter Email-ID"
                     name="email"
+                    required
+                    onChange={emailHandler}
                   />
                 </div>
                 <div className="input-container">
@@ -52,47 +148,51 @@ function Signup() {
                     type="password"
                     placeholder="Password"
                     name="password"
+                    required
                   />
                 </div>
                 <div className="input-container">
                   <RiLockPasswordLine size="25px" color="#5B5B5B" />
                   <input
+                    id="cpassword"
                     type="password"
                     placeholder="Confirm Password"
                     name="cpassword"
+                    required
+                    // onChange={passwordHandler}
                   />
                 </div>
                 <div className="selectElement">
                   <div className="input-container select-college">
                     <BsFillHouseUpFill size="20px" color="#5B5B5B" />
-                    <select className="clg">
-                      <option>Select Colleges</option>
-                      <option>VBU</option>
-                      <option>St. colomba's</option>
+                    <select className="clg" name="college" required>
+                      <option value="select">Select Colleges</option>
+                      <option value="vbu">VBU</option>
+                      <option value="St.colomba's">St. colomba's</option>
                     </select>
                   </div>
 
                   <div className="course-sem">
                     <div className="input-container select-courses">
                       <MdSchool size="25px" color="#5B5B5B" />
-                      <select className="clg">
-                        <option>Courses</option>
-                        <option>BCA</option>
-                        <option>MCA</option>
+                      <select className="clg" name="course" required>
+                        <option value="">Courses</option>
+                        <option value="BCA">BCA</option>
+                        <option value="MCA">MCA</option>
                       </select>
                     </div>
 
                     <div className="input-container select-semester">
                       <MdSchool size="25px" color="#5B5B5B" />
 
-                      <select className="clg">
+                      <select className="clg" name="sem" required>
                         <option>Semester</option>
-                        <option>1</option>
-                        <option>2</option>
-                        <option>3</option>
-                        <option>4</option>
-                        <option>5</option>
-                        <option>6</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
                       </select>
                     </div>
                   </div>
@@ -105,7 +205,13 @@ function Signup() {
                 </div>{" "}
                 <br />
                 <div className="register-button">
-                  <button className="register-btn">Register</button>
+                  <button
+                    className="register-btn"
+                    name="register"
+                    value="Register"
+                  >
+                    Register
+                  </button>
                 </div>
               </form>
               <p className="signup-footer">
